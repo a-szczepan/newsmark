b0e4378a6707021d37d3e22a39748dc5bbcbb579
Angelika <szczepanczykangelika@gmail.com>
2023-06-25 15:07:29 +0200

    add: storybook

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,36 +1,24 @@
 module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2021": true
+  },
+  "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:storybook/recommended"],
+  "overrides": [{
     "env": {
-        "browser": true,
-        "node": true,
-        "es2021": true
+      "node": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
+    "files": [".eslintrc.{js,cjs}"],
     "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "rules": {
+      "sourceType": "script"
     }
-}
+  }],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": ["@typescript-eslint", "react"],
+  "rules": {}
+};
