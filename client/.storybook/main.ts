8cb6bc45e2b008677a89b7f7e4f222c9527cbc1d
Angelika <szczepanczykangelika@gmail.com>
2023-06-25 18:16:34 +0200

    add: addons and cleanup

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -4,7 +4,9 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/preset-scss',
+    '@storybook/addon-a11y'
   ],
   framework: {
     name: '@storybook/react-webpack5',
