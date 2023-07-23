4db177123d7ea5db7bfeca75d531570e6bd1f0ad
Angelika Szczepaczyk <szczepanczykangelika@gmail.com>
2023-07-23 17:28:23 +0200

    add: global layout component

---
+++ client/src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import classnames from 'classnames';
+import styles from './Layout.module.scss';
+
+type LayoutProps = {
+    children: React.ReactNode
+  }
+
+export const Layout: React.FC<LayoutProps> = ({children}): JSX.Element => {
+    return <div className={classnames(styles.layout)}>{children}</div>;
+  };
+  
