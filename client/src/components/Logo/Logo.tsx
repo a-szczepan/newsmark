44eeda2f793dd45b1d61353043c5ae34420330ce
Angelika Szczepaczyk <szczepanczykangelika@gmail.com>
2023-10-31 12:25:28 +0100

    fix: render issues

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './Logo.module.scss';
 import { ReactSVG } from 'react-svg';
-import  LogoImg  from '../../assets/images/Newsmark.svg';
+import LogoImg from '../../assets/images/Newsmark.svg';
 import { redirect } from 'react-router-dom';
 
 export const Logo: React.FC = () => {
   return (
-    <a href="/" onClick={() => redirect('/')}>
+    <a href='/' onClick={() => redirect('/')}>
       <ReactSVG src={LogoImg} className={styles.logo} />
     </a>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { RouterProvider } from 'react-router-dom';
@@ -9,11 +9,14 @@ import '../src/styles/_global.scss';
 import '../src/styles/variables.css';
 import { Layout } from './components/Layout/Layout';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <Layout>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </Layout>,
-  document.getElementById('root')
+  </Layout>
 );
