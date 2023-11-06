05d1f00d26c334f50ce9a27a2d904b13bb322688
Angelika Szczepaczyk <szczepanczykangelika@gmail.com>
2023-11-06 18:42:49 +0100

    fix: react router in storybook

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -14,6 +14,7 @@ const config: StorybookConfig = {
     '@storybook/addon-designs',
     "storybook-addon-pseudo-states",
     'storybook-css-modules',
+    'storybook-addon-react-router-v6',
     {
       name: '@storybook/addon-styling',
       options: {
--- client/src/stories/pages/LoginPage.stories.tsx
+++ client/src/stories/pages/LoginPage.stories.tsx
@@ -4,6 +4,10 @@ import { paddingDecorator } from '../decorators';
 import Login from '../../pages/Login/Login';
 import { Provider } from 'react-redux';
 import store from '../../store/store';
+import {
+  reactRouterParameters,
+  withRouter
+} from 'storybook-addon-react-router-v6';
 
 const meta = {
   title: 'Pages/LoginPage',
@@ -13,15 +17,21 @@ const meta = {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/QsqqfX07v3j9m5FUppaZQd/Newsmark?type=design&node-id=228-3&mode=design&t=fK2CNfHDQwCCxnmz-0'
-    }
+    },
+    reactRouter: reactRouterParameters({
+      routing: { path: '/login' }
+    })
   },
   decorators: [
-    (Story) => (
-      <Provider store={store}>
-        <Story />
-      </Provider>
-    ),
-    paddingDecorator
+    (Story) => {
+      return (
+        <Provider store={store}>
+          <Story />
+        </Provider>
+      );
+    },
+    // paddingDecorator,
+    withRouter
   ]
 } satisfies Meta<typeof Login>;
 
