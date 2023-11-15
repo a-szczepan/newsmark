226900210bb4d3fe632dd4e6d956725b56cce919
Angelika Szczepaczyk <szczepanczykangelika@gmail.com>
2023-11-15 11:05:44 +0100

    add: mobile modal panel

---
+++ client/src/pages/ArticlePage/MobileModal.tsx
@@ -0,0 +1,54 @@
+import { createPortal } from 'react-dom';
+import classnames from 'classnames';
+import { Dispatch, PropsWithChildren, SetStateAction, useEffect, useState } from 'react';
+import styles from './MobileModal.module.scss';
+import { IconButton } from '../../components/Button/Button';
+import { IconType } from '../../components/Icon/Icon';
+
+type MobileModalProps = PropsWithChildren<{
+  isOpened: boolean;
+  setIsOpened: Dispatch<SetStateAction<boolean>>
+  classes?: string[];
+}>;
+
+export const MobileModal: React.FC<MobileModalProps> = ({
+  isOpened,
+  setIsOpened,
+  children
+}) => {
+  //   const [isOpened, setIsOpened] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  useEffect(() => {
+    if (isOpened) document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'scroll';
+    };
+  }, [isOpened]);
+
+  return (
+    <>
+      {isOpened &&
+        createPortal(
+          <div className={styles.underlay}>
+            <div
+              className={classnames(styles.modal, {
+                [styles.closing]: !isOpened
+              })}
+            >
+              <IconButton
+                buttonAction={() => {
+                  setIsOpened(false);
+                }}
+                icon={IconType.close}
+                lightVariant
+                classes={[styles.closeModalIcon]}
+              />
+              {children}
+            </div>
+          </div>,
+          document.getElementById('portal-root')!
+        )}
+    </>
+  );
+};
