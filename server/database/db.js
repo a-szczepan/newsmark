2625528afea7cd46ec1886c633aec23509949573
Angelika Szczepaczyk <szczepanczykangelika@gmail.com>
2024-02-12 07:10:02 +0100

    update: change dbs deployment provider

--- server/database/db.js
+++ server/database/db.js
@@ -1,57 +1,48 @@
-const fs = require("fs");
-const mysql = require("mysql2/promise");
-const path = require("path");
-const basename = path.basename(__filename);
-const modelsDir = path.resolve() + "\/models";
-const { Sequelize } = require("sequelize");
-const db = {};
+const fs = require('fs')
+const mysql = require('mysql2/promise')
+const path = require('path')
+const basename = path.basename(__filename)
+const modelsDir = path.resolve() + '/models'
+const { Sequelize } = require('sequelize')
+const db = {}
 
-const sequelize = new Sequelize(
-  "newsmark",
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true,
-      },
-    },
+const sequelize = new Sequelize('railway', process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false //free db deploy tier
+    }
   }
-);
+})
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.')
   })
   .catch((error) => {
-    console.error("Unable to connect db: ", error);
-  });
+    console.error('Unable to connect db: ', error)
+  })
 
 fs.readdirSync(modelsDir)
   .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
+    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
   })
   .forEach((file) => {
-    const model = require(path.join(modelsDir, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
+    const model = require(path.join(modelsDir, file))(sequelize, Sequelize.DataTypes)
+    db[model.name] = model
+  })
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
-    db[modelName].associate(db);
-    console.log(db[modelName], "associated", db);
+    db[modelName].associate(db)
+    console.log(db[modelName], 'associated', db)
   }
-});
+})
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
-module.exports = db;
+module.exports = db
