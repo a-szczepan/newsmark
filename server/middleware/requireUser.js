46c1eb01009fba80c5bf24dc2a85a4322c8ab83c
Angelika Szczepaczyk <szczepanczykangelika@gmail.com>
2024-02-13 19:30:14 +0100

    test

--- server/middleware/requireUser.js
+++ server/middleware/requireUser.js
@@ -1,11 +1,11 @@
 function requireUser(req, res, next) {
-  const user = res?.locals?.user;
-
+  const user = res?.locals?.user
+  console.log(res.locals, 'require user res locals')
   if (!user) {
-    return res.sendStatus(403);
+    return res.sendStatus(403)
   }
 
-  return next();
+  return next()
 }
 
-module.exports = { requireUser };
+module.exports = { requireUser }
